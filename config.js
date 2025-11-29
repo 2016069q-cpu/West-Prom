a4f141f7962f8e7855ea10a946fa83f8226a19df
2016069q-cpu <2016069q@gmail.com>
2025-11-29 16:52:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Настройки — заменён SCRIPT_URL по запросу пользователя
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxz4ULAoJHutt5zaDgyDwIgY2jwp_dAA5fZqfmpJCV008aHWY135iDCpvDu756hN_LMkw/exec"; // https://script.google.com/macros/s/XXX/exec
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzZxgBpS3ndf-k1p6X0sYyz8ERrG2-fELOSehUUV7cl0x5PEuFuIiTeFd_G9rOhretaVA/exec"; // https://script.google.com/macros/s/XXX/exec
 const ADMIN_PASSWORD = "westprom123"; // поменяй на сложный пароль при необходимости
